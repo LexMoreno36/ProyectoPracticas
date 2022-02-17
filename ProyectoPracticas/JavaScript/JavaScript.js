843369b92d0a965cb3865e4720e004dfec4bbca7
LexMoreno36 <alex.moreno.palacio@gmail.com>
2022-02-17 13:33:32 +0100

    Configuración Firebase y API request

--- ProyectoPracticas/JavaScript/JavaScript.js
+++ ProyectoPracticas/JavaScript/JavaScript.js
@@ -1,1 +1,46 @@
-﻿var url = "https://jsonplaceholder.typicode.com/";
+﻿//Data Structure
+class Issue {
+    constructor(
+        id = '',
+        title = '',
+        description = '',
+        severity = '',
+        status = '',
+        asignee = ''
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.severity = severity;
+        this.status = status;
+        this.asignee = asignee;
+    }
+}
+class IssueLibrary {
+    constructor() { this.issues = [] }
+
+    addIssue(newIssue) {
+        this.issues.push(newBook)
+    }
+    removeIssue(id) {
+        this.issues = this.issues.filter((issue) => issue.id !== id)
+    }
+    getIssue(id) {
+        return this.issues.find((issue) => issue.id === id)
+    }
+}
+
+const IssueLibrary = new IssueLibrary()
+
+
+
+//Util
+const IssueToDoc = (issue) => {
+    return {
+        title: book.title,
+        author: book.author,
+        pages: book.pages,
+        isRead: book.isRead,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    }
+}
---
+++ ProyectoPracticas/Server/index.js
@@ -0,0 +1,16 @@
+﻿const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8083 });
+
+wss.on("connection", ws => {
+    console.log("New client connected!")
+
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${ data }`);
+    })
+
+    ws.on("close", () => {
+        console.log("Client disconnected")
+    })
+
+})
